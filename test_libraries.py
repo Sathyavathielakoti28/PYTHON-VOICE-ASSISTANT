d508a8a6ca7a3895d88340fb169b263617a7ce4e
Sathyavathi Elakoti <sathyavathielakoti@gmail.com>
2026-07-13 22:34:44 +0530

    Delete test_libraries.py

--- test_libraries.py
+++
@@ -1,9 +0,0 @@
-import speech_recognition as sr
-import pyttsx3
-import pywhatkit
-import pyaudio
-print("SpeechRecognition imported successfully")
-print("pyttsx3 imported successfully")
-print("pywhatkit imported successfully")
-print("PyAudio imported successfully")
-print("\n All libraries are installed correctly!")
